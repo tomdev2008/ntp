a13c0a5d4956c09b45467581c97806073f5c8749
lenovo <lenovo@10.200.27.161>
2013-10-18 16:47:26 +0800

    新增常量定义

---
+++ src/main/java/cn/me/xdf/model/base/Constant.java
@@ -0,0 +1,104 @@
+package cn.me.xdf.model.base;
+
+public class Constant {
+	/**
+	 * 素材类型：
+	 * 01视频
+	 */
+	public static final String MATERIAL_TYPE_VIDEO = "01";
+	/**
+	 * 素材类型：
+	 * 02音频
+	 */
+	public static final String MATERIAL_TYPE_AUDIO = "02";
+	/**
+	 * 素材类型：
+	 * 03图片
+	 */
+	public static final String MATERIAL_TYPE_PIC = "03";
+	/**
+	 * 素材类型：
+	 * 04文档
+	 */
+	public static final String MATERIAL_TYPE_DOC = "04";
+	/**
+	 * 素材类型：
+	 * 05幻灯片
+	 */
+	public static final String MATERIAL_TYPE_PPT = "05";
+	/**
+	 * 素材类型：
+	 * 06网页(链接)
+	 */
+	public static final String MATERIAL_TYPE_HTML = "06";
+	/**
+	 * 素材类型：
+	 * 07富文本
+	 */
+	public static final String MATERIAL_TYPE_RICHTEXT  = "07";
+	/**
+	 * 素材类型：
+	 * 08测试
+	 */
+	public static final String MATERIAL_TYPE_TEST = "08";
+	/**
+	 * 素材类型：
+	 * 09测评
+	 */
+	public static final String MATERIAL_TYPE_ASSESSMENT = "09";
+	/**
+	 * 素材类型：
+	 * 10作业包
+	 */
+	public static final String MATERIAL_TYPE_JOBPACKAGE = "10";
+	/**
+	 * 素材类型：
+	 * 11日程
+	 */
+	public static final String MATERIAL_TYPE_SCHEDULE = "11";
+	
+	/**
+	 * 试题类型：
+	 * 1单选
+	 */
+	public static final Integer EXAM_QUESTION_SINGLE_SELECTION = 1;
+	
+	/**
+	 * 试题类型：
+	 * 2多选
+	 */
+	public static final Integer EXAM_QUESTION_MULTIPLE_SELECTION = 2;
+	
+	/**
+	 * 试题类型：
+	 * 3填空
+	 */
+	public static final Integer EXAM_QUESTION_CLOZE = 3;
+	
+	/**
+	 * 存储方式：
+	 * 01数据库
+	 */
+	public static final String STORE_TYPE_DB = "01";
+	
+	/**
+	 * 存储方式：
+	 * 02磁盘
+	 */
+	public static final String STORE_TYPE_DISK = "02";
+	
+	/**
+	 * 消息类型：01评论
+	 */
+	public static final String MESSAGE_TYPE_REVIEW = "01";
+	
+	/**
+	 * 消息类型：02备课心情
+	 */
+	public static final String MESSAGE_TYPE_MOOD = "02";
+	
+	/**
+	 * 消息类型：03系统消息
+	 */
+	public static final String MESSAGE_TYPE_SYS = "03";
+}
