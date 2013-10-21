253be6b7a1fc8933f8b523d8681c43839352aadc
xiaobin268 <xiaobin268@xiaobin>
2013-10-21 14:55:15 +0800

    增加Action包

---
+++ src/main/java/cn/me/xdf/action/common/UploadFileController.java
@@ -0,0 +1,13 @@
+package cn.me.xdf.action.common;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 
+ * @author xiaobin
+ *
+ */
+@Controller
+public class UploadFileController {
+
+}
