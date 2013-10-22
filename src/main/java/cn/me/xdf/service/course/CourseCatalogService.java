18e0a5bed9dbcb72b70c4c289f19e748793119c0
lenovo <lenovo@10.200.27.161>
2013-10-22 12:00:14 +0800

    添加了课程模板新增页面及章节service

---
+++ src/main/java/cn/me/xdf/service/course/CourseCatalogService.java
@@ -0,0 +1,37 @@
+package cn.me.xdf.service.course;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import cn.me.xdf.common.hibernate4.Finder;
+import cn.me.xdf.model.course.CourseCatalog;
+import cn.me.xdf.service.BaseService;
+
+@Service
+@Transactional(readOnly = false)
+public class CourseCatalogService extends BaseService{
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public  Class<CourseCatalog> getEntityClass() {
+		return CourseCatalog.class;
+	}
+	
+	/**
+	 * 查找课程权限
+	 * @param courseId 课程ID
+	 * @return List 章节列表
+	 */
+	@Transactional(readOnly = true)
+	public List<CourseCatalog> getCatalogsByCourseId(String courseId){
+		//根据课程ID查找章节，并按总序号升序
+		Finder finder = Finder
+				.create("from CourseCatalog catalog ");
+		finder.append("where catalog.courseInfo.fdId = :userId order by catalog.fdTotalNo");
+		finder.setParam("courseId", courseId);		
+		return  super.find(finder);
+	}
+
+}
