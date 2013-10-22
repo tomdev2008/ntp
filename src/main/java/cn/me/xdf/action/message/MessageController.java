18c9afdd3ed10c5184be6135784f4ff57e30702a
zq <zq@10.200.27.52>
2013-10-22 14:52:00 +0800

    增加消息、评分action

---
+++ src/main/java/cn/me/xdf/action/message/MessageController.java
@@ -0,0 +1,47 @@
+package cn.me.xdf.action.message;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.me.xdf.service.message.MessageService;
+
+/**
+ * 消息
+ * 
+ * @author
+ * 
+ */
+@Controller
+@RequestMapping(value = "/message")
+@Scope("request")
+public class MessageController {
+
+	@Autowired
+	private MessageService messageService ;
+	
+	/**
+	 * 支持或反对评论
+	 * 
+	 * @return 支持数和反对数，格式：支持数_反对数（例：12_11）
+	 */
+	@RequestMapping(value = "ajax/supportOrOpposeMessage")
+	@ResponseBody
+	public String supportOrOpposeMessage(String userId, String messageId,String fdType) {
+		return messageService.supportOrOpposeMessage(userId, messageId, fdType);
+	}
+	
+	/**
+	 * 根据消息ID删除消息信息
+	 * 
+	 */
+	@RequestMapping(value = "ajax/deleteMessageById")
+	@ResponseBody
+	public void deleteMessageById(String fdid) {
+		messageService.delete(fdid);
+	}
+}
