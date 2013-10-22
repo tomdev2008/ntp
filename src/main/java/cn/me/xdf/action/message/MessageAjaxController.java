43ad673c84483e3183c793948ff751b058e03d65
zq <zq@10.200.27.52>
2013-10-22 18:20:23 +0800

    修改了action命名

---
+++ src/main/java/cn/me/xdf/action/message/MessageAjaxController.java
@@ -0,0 +1,56 @@
+package cn.me.xdf.action.message;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.me.xdf.service.message.MessageService;
+
+/**
+ * 消息
+ * 
+ * @author
+ * 
+ */
+@Controller
+@RequestMapping(value = "/message/ajax")
+@Scope("request")
+public class MessageAjaxController {
+
+	@Autowired
+	private MessageService messageService ;
+	
+	/**
+	 * 支持或反对评论
+	 * 
+	 * @return 支持数和反对数，格式：支持数_反对数（例：12_11）
+	 */
+	@RequestMapping(value = "supportOrOpposeMessage")
+	@ResponseBody
+	public String supportOrOpposeMessage(String userId, String messageId,String fdType) {
+		return messageService.supportOrOpposeMessage(userId, messageId, fdType);
+	}
+	
+	/**
+	 * 根据消息ID删除消息信息
+	 * 
+	 */
+	@RequestMapping(value = "deleteMessageById")
+	@ResponseBody
+	public void deleteMessageById(String fdid) {
+		messageService.delete(fdid);
+	}
+	
+	/**
+	 * 查看用户是否可以支持或反对评论
+	 * 
+	 * @return String(true:能；false：不能)
+	 */
+	@RequestMapping(value = "canSupportOrOppose")
+	@ResponseBody
+	public String canSupportOrOppose(String userId, String messageId) {
+		return messageService.canSupportOrOppose(userId, messageId)+"";
+	}
+}
