14146d8955a09e11e6d05276dd7e875ab8bc9bcb
zq <zq@10.200.27.52>
2013-10-25 15:17:29 +0800

    课程授权代码

--- src/main/java/cn/me/xdf/service/SysOrgPersonService.java
+++ src/main/java/cn/me/xdf/service/SysOrgPersonService.java
@@ -1,8 +1,11 @@
 package cn.me.xdf.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import cn.me.xdf.common.hibernate4.Finder;
 import cn.me.xdf.model.organization.SysOrgElement;
 import cn.me.xdf.model.organization.SysOrgPerson;
 
@@ -13,7 +16,23 @@ public class SysOrgPersonService extends BaseService{
 	@SuppressWarnings("unchecked")
 	@Override
 	public Class<SysOrgPerson> getEntityClass() {
-		return SysOrgPerson.class;
+		return SysOrgPerson.class; 
+	}
+	
+	/**
+	 * 根据登录名称和姓名模糊查询用户(前10条)
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public List<SysOrgPerson> findUserByLinkLoginAndRealNameTop10(String key){
+		Finder finder = Finder
+				.create("from SysOrgPerson p ");
+		finder.append("where p.loginName like :key1  or  p.notifyEntity.realName like :key2");
+		finder.setParam("key1", "%"+key+"%");
+		finder.setParam("key2", "%"+key+"%");
+		return (List<SysOrgPerson>)getPage(finder, 1,10).getList();
+		
 	}
 
 }
