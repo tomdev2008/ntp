fc0b8f877c9a2328ada6b6f132ac9b1566ff1014
xiaobin268 <xiaobin268@gmail.com>
2013-10-29 16:24:05 +0800

    Json针对Hibernate的Model的插件---test

---
+++ src/main/java/cn/me/xdf/model/demo/BamScore.java
@@ -0,0 +1,90 @@
+package cn.me.xdf.model.demo;
+
+import cn.me.xdf.common.json.JsonUtils;
+import cn.me.xdf.model.base.BamProcess;
+import cn.me.xdf.model.base.IdEntity;
+import cn.me.xdf.model.course.CourseCatalog;
+import cn.me.xdf.model.course.CourseInfo;
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+import java.util.List;
+
+/**
+ *
+ *
+ * Created with IntelliJ IDEA.
+ * User: xiaobin268
+ * Date: 13-10-29
+ * Time: 下午3:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class BamScore extends IdEntity implements BamProcess {
+
+    /**
+     * 对应课程的信息
+     */
+    private CourseInfo courseInfo;
+
+    /**
+     * 章节的json串
+     */
+    private String catalogJson;
+
+    /**
+     * 章节与素材的关系JSON串
+     */
+    private String courseContentJson;
+
+    private boolean through;
+
+
+    public CourseInfo getCourseInfo() {
+        return courseInfo;
+    }
+
+    public void setCourseInfo(CourseInfo courseInfo) {
+        this.courseInfo = courseInfo;
+    }
+
+    public String getCatalogJson() {
+        return catalogJson;
+    }
+
+    public void setCatalogJson(String catalogJson) {
+        this.catalogJson = catalogJson;
+    }
+
+    public String getCourseContentJson() {
+        return courseContentJson;
+    }
+
+    public void setCourseContentJson(String courseContentJson) {
+        this.courseContentJson = courseContentJson;
+    }
+
+    @Override
+    public boolean getThrough() {
+        return through;
+    }
+
+    @Override
+    public void setThrough(boolean through) {
+        this.through = through;
+    }
+
+    /**
+     * 获取课程流程里所有章节
+     *
+     * @return
+     */
+    @JsonIgnore
+    public List<CourseCatalog> getCatalogs() {
+        if (StringUtils.isNotBlank(catalogJson)) {
+            return (List<CourseCatalog>) JsonUtils.readObjectByJson(catalogJson, List.class);
+        }
+        return null;
+    }
+
+
+}
