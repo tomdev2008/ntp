7f3742d3f77fcd7d8f99bf113713306a031c8e6e
hanhl <hanhl@mypc>
2013-12-25 18:35:47 +0800

    修改

--- src/main/java/cn/me/xdf/action/HeadPageAjaxContoller.java
+++ src/main/java/cn/me/xdf/action/HeadPageAjaxContoller.java
@@ -3,13 +3,15 @@ package cn.me.xdf.action;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HeadPageContoller {
+public class HeadPageAjaxContoller {
 	@RequestMapping(value = "getSchoolInfo")
-	public String getSchoolInfo() {
+	public String getSchoolInfo(HttpServletRequest request) {
 		String sql = "select count(*) from sys_org_element  o where o.fd_parentid='142eac9fb5cac1c910431544a39ba22a'";// 学校数量
 
 		String sql1 = "select count(*) from sys_user_role where person_role='guidance'"; // 导师数量
@@ -19,10 +21,15 @@ public class HeadPageContoller {
 		String sql3 = "select count(*) from ixdf_ntp_course c where c.isavailable='Y' ";// --课程数量
 		
 		Map schoolinfo=new HashMap();
-		schoolinfo.put("scnum", "");
-		schoolinfo.put("thnum", "");
-		schoolinfo.put("mnum", "");
-		schoolinfo.put("cnum", "");
+		schoolinfo.put("countShool", "");
+		schoolinfo.put("countTeacher", "");
+		schoolinfo.put("countNewTeacher", "");
+		schoolinfo.put("countCourse", "");
 		return "";
+	}
+	
+	@RequestMapping(value="getTeacherWord")
+	public String getTeacherWord(HttpServletRequest request) {
+		return "" ;
 	}
 }
