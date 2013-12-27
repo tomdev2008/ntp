77a7107acb0b7c173d3195f09159fb3f1762db72
yuhuizhe <yuhz@landray.com.cn>
2013-12-27 19:31:26 +0800

    系列的删除

--- src/main/java/cn/me/xdf/model/course/SeriesInfo.java
+++ src/main/java/cn/me/xdf/model/course/SeriesInfo.java
@@ -7,8 +7,6 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
