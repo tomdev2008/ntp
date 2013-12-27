b155e4c83f96078e19e39d55d94be70d3551ad25
lenovo <lenovo@10.200.27.161>
2013-12-27 11:22:40 +0800

    修改系统管理模块问题

--- src/main/java/cn/me/xdf/action/base/GroupContorller.java
+++ src/main/java/cn/me/xdf/action/base/GroupContorller.java
@@ -1,7 +1,9 @@
 package cn.me.xdf.action.base;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,7 +17,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import cn.me.xdf.common.hibernate4.Finder;
 import cn.me.xdf.common.page.Pagination;
+import cn.me.xdf.model.organization.SysOrgElement;
 import cn.me.xdf.model.organization.SysOrgGroup;
+import cn.me.xdf.model.organization.SysOrgPerson;
 import cn.me.xdf.model.organization.UserRole;
 import cn.me.xdf.service.SysOrgPersonService;
 
@@ -58,6 +62,20 @@ public class GroupContorller {
 		model.addAttribute("active", "group");
 		SysOrgGroup group = sysOrgPersonService.load(SysOrgGroup.class, id);
 		model.addAttribute("bean", group);
+		List<Map> res = new ArrayList<Map>();
+		List<SysOrgElement> elements = group.getFdMembers();
+		for(SysOrgElement element:elements){
+			Map map = new HashMap();
+			map.put("fdName", element.getFdName());
+			if(element.getFdOrgType()==element.ORG_TYPE_PERSON){
+				SysOrgPerson person = sysOrgPersonService.get(element.getFdId());
+				if(person!=null){
+					map.put("fdName", map.get("fdName")+"（"+person.getFdEmail()+"）");
+				}
+			}
+			res.add(map);
+		}
+		model.addAttribute("res", res);
 		return "/admin/group/view";
 	}
 }
